c20c679d2748bc0db382f9ba7bd738a93da4c806
DESKTOP-9MENRTJ\ymsong <ssupersam13@gmail.com>
2024-05-07 20:55:12 +0900

    fix : basecharacterHUD 유령 퇴치

---
+++ Assets/Scripts/Character/BaseCharacterHP.cs
@@ -0,0 +1,24 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterHP : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer hpBar;
+
+    private void Start()
+    {
+        BaseCharacter owner = GetComponent<BaseCharacter>();
+        owner.Health.OnHealthChanged += UpdateHPBar;
+    }
+
+    public void UpdateHPBar(BaseCharacter _character)
+    {
+        float curHealth = _character.Health.CurHealth;
+        float maxHealth = _character.Health.MaxHealth;
+        float nextScale = curHealth / maxHealth;
+        hpBar.transform.DOScaleX(nextScale, 1f).SetEase(Ease.OutCubic);
+    }
+
+}
