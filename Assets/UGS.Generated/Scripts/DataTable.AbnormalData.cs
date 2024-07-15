a70e5b7ec1a346f242694f6bf0375dc5d5fc748e
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-07-16 05:46:37 +0900

    add : Unity Google Spread Sheet 연동 라이브러리

---
+++ Assets/UGS.Generated/Scripts/DataTable.AbnormalData.cs
@@ -0,0 +1,259 @@
+
+/*     ===== Do not touch this. Auto Generated Code. =====    */
+/*     If you want custom code generation modify this => 'CodeGeneratorUnityEngine.cs'  */
+using GoogleSheet.Protocol.v2.Res;
+using GoogleSheet.Protocol.v2.Req;
+using UGS;
+using System;
+using UGS.IO;
+using GoogleSheet;
+using System.Collections.Generic;
+using System.IO;
+using GoogleSheet.Type;
+using System.Reflection;
+using UnityEngine;
+
+
+namespace DataTable
+{
+    [GoogleSheet.Attribute.TableStruct]
+    public partial class AbnormalData : ITable
+    { 
+
+        public delegate void OnLoadedFromGoogleSheets(List<AbnormalData> loadedList, Dictionary<int, AbnormalData> loadedDictionary);
+
+        static bool isLoaded = false;
+        static string spreadSheetID = "1rPi9VXSVtBWKZ3HgJj3M1M_aj6aUQog6OZPt7fIUCUs"; // it is file id
+        static string sheetID = "1828310309"; // it is sheet id
+        static UnityFileReader reader = new UnityFileReader();
+
+/* Your Loaded Data Storage. */
+    
+        public static Dictionary<int, AbnormalData> AbnormalDataMap = new Dictionary<int, AbnormalData>();  
+        public static List<AbnormalData> AbnormalDataList = new List<AbnormalData>();   
+
+        /// <summary>
+        /// Get AbnormalData List 
+        /// Auto Load
+        /// </summary>
+        public static List<AbnormalData> GetList()
+        {{
+           if (isLoaded == false) Load();
+           return AbnormalDataList;
+        }}
+
+        /// <summary>
+        /// Get AbnormalData Dictionary, keyType is your sheet A1 field type.
+        /// - Auto Load
+        /// </summary>
+        public static Dictionary<int, AbnormalData>  GetDictionary()
+        {{
+           if (isLoaded == false) Load();
+           return AbnormalDataMap;
+        }}
+
+    
+
+/* Fields. */
+
+		public System.Int32 ID;
+		public System.String abnormalName;
+		public System.Int32 cost;
+  
+
+#region fuctions
+
+
+        public static void Load(bool forceReload = false)
+        {
+            if(isLoaded && forceReload == false)
+            {
+#if UGS_DEBUG
+                 Debug.Log("AbnormalData is already loaded! if you want reload then, forceReload parameter set true");
+#endif
+                 return;
+            }
+
+            string text = reader.ReadData("DataTable"); 
+            if (text != null)
+            {
+                var result = Newtonsoft.Json.JsonConvert.DeserializeObject<ReadSpreadSheetResult>(text);
+                CommonLoad(result.jsonObject, forceReload); 
+                if(!isLoaded)isLoaded = true;
+            }
+      
+        }
+ 
+
+        public static void LoadFromGoogle(System.Action<List<AbnormalData>, Dictionary<int, AbnormalData>> onLoaded, bool updateCurrentData = false)
+        {      
+                IHttpProtcol webInstance = null;
+    #if UNITY_EDITOR
+                if (Application.isPlaying == false)
+                {
+                    webInstance = UnityEditorWebRequest.Instance as IHttpProtcol;
+                }
+                else 
+                {
+                    webInstance = UnityPlayerWebRequest.Instance as IHttpProtcol;
+                }
+    #endif
+    #if !UNITY_EDITOR
+                     webInstance = UnityPlayerWebRequest.Instance as IHttpProtcol;
+    #endif
+          
+ 
+                var mdl = new ReadSpreadSheetReqModel(spreadSheetID);
+                webInstance.ReadSpreadSheet(mdl, OnError, (data) => {
+                    var loaded = CommonLoad(data.jsonObject, updateCurrentData); 
+                    onLoaded?.Invoke(loaded.list, loaded.map);
+                });
+        }
+
+               
+
+
+    public static (List<AbnormalData> list, Dictionary<int, AbnormalData> map) CommonLoad(Dictionary<string, Dictionary<string, List<string>>> jsonObject, bool forceReload){
+            Dictionary<int, AbnormalData> Map = new Dictionary<int, AbnormalData>();
+            List<AbnormalData> List = new List<AbnormalData>();     
+            TypeMap.Init();
+            FieldInfo[] fields = typeof(AbnormalData).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            List<(string original, string propertyName, string type)> typeInfos = new List<(string, string, string)>(); 
+            List<List<string>> rows = new List<List<string>>();
+            var sheet = jsonObject["AbnormalData"];
+
+            foreach (var column in sheet.Keys)
+            {
+                string[] split = column.Replace(" ", null).Split(':');
+                         string column_field = split[0];
+                string   column_type = split[1];
+
+                typeInfos.Add((column, column_field, column_type));
+                          List<string> typeValues = sheet[column];
+                rows.Add(typeValues);
+            }
+
+          // 실제 데이터 로드
+                    if (rows.Count != 0)
+                    {
+                        int rowCount = rows[0].Count;
+                        for (int i = 0; i < rowCount; i++)
+                        {
+                            AbnormalData instance = new AbnormalData();
+                            for (int j = 0; j < typeInfos.Count; j++)
+                            {
+                                try
+                                {
+                                    var typeInfo = TypeMap.StrMap[typeInfos[j].type];
+                                    //int, float, List<..> etc
+                                    string type = typeInfos[j].type;
+                                    if (type.StartsWith(" < ") && type.Substring(1, 4) == "Enum" && type.EndsWith(">"))
+                                    {
+                                         Debug.Log("It's Enum");
+                                    }
+
+                                    var readedValue = TypeMap.Map[typeInfo].Read(rows[j][i]);
+                                    fields[j].SetValue(instance, readedValue);
+
+                                }
+                                catch (Exception e)
+                                {
+                                    if (e is UGSValueParseException)
+                                    {
+                                        Debug.LogError("<color=red> UGS Value Parse Failed! </color>");
+                                        Debug.LogError(e);
+                                        return (null, null);
+                                    }
+
+                                    //enum parse
+                                    var type = typeInfos[j].type;
+                                    type = type.Replace("Enum<", null);
+                                    type = type.Replace(">", null);
+
+                                    var readedValue = TypeMap.EnumMap[type].Read(rows[j][i]);
+                                    fields[j].SetValue(instance, readedValue); 
+                                }
+                              
+                            }
+                            List.Add(instance); 
+                            Map.Add(instance.ID, instance);
+                        }
+                        if(isLoaded == false || forceReload)
+                        { 
+                            AbnormalDataList = List;
+                            AbnormalDataMap = Map;
+                            isLoaded = true;
+                        }
+                    } 
+                    return (List, Map); 
+}
+
+
+ 
+
+        public static void Write(AbnormalData data, System.Action<WriteObjectResult> onWriteCallback = null)
+        { 
+            TypeMap.Init();
+            FieldInfo[] fields = typeof(AbnormalData).GetFields(BindingFlags.Public | BindingFlags.Instance);
+            var datas = new string[fields.Length];
+            for (int i = 0; i < fields.Length; i++)
+            {
+                var type = fields[i].FieldType;
+                string writeRule = null;
+                if(type.IsEnum)
+                {
+                    writeRule = TypeMap.EnumMap[type.Name].Write(fields[i].GetValue(data));
+                }
+                else
+                {
+                    writeRule = TypeMap.Map[type].Write(fields[i].GetValue(data));
+                } 
+                datas[i] = writeRule; 
+            }  
+           
+#if UNITY_EDITOR
+if(Application.isPlaying == false)
+{
+                UnityPlayerWebRequest.Instance.WriteObject(new WriteObjectReqModel(spreadSheetID, sheetID, datas[0], datas), OnError, onWriteCallback);
+
+}
+else
+{
+            UnityPlayerWebRequest.Instance.WriteObject(new  WriteObjectReqModel(spreadSheetID, sheetID, datas[0], datas), OnError, onWriteCallback);
+
+}
+#endif
+
+#if !UNITY_EDITOR
+   UnityPlayerWebRequest.Instance.WriteObject(new  WriteObjectReqModel(spreadSheetID, sheetID, datas[0], datas), OnError, onWriteCallback);
+
+#endif
+        } 
+          
+
+ 
+
+
+#endregion
+
+#region OdinInsepctorExtentions
+#if ODIN_INSPECTOR
+    [Sirenix.OdinInspector.Button("UploadToSheet")]
+    public void Upload()
+    {
+        Write(this);
+    }
+ 
+    
+#endif
+
+
+ 
+#endregion
+    public static void OnError(System.Exception e){
+         UnityGoogleSheet.OnTableError(e);
+    }
+ 
+    }
+}
+        
