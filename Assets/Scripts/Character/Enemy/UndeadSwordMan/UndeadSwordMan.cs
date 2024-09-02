22ffcbc36c9cf9176e594e3ecb5eec26fa9b1a8f
ymsong <ssupersam13@gmail.com>
2024-09-02 15:11:15 +0900

    add : 불사병 애니메이션 업로드 완료 fix : 해태 죄악환영 스킬 사용 안되던 버그 수정

--- Assets/Scripts/Character/Enemy/UndeadSwordMan/UndeadSwordMan.cs
+++ Assets/Scripts/Character/Enemy/UndeadSwordMan/UndeadSwordMan.cs
@@ -1,9 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class UndeadSwordMan : BaseEnemy
 {
+    public AnimatorController controller1;
+    public AnimatorController controller2;
+
+    private void Start()
+    {
+        Animator animator = GetComponent<Animator>();
+        int randomValue = UnityEngine.Random.Range(0, 2); // 0 또는 1을 랜덤으로 생성
+        if(randomValue == 0)
+        {
+            animator.runtimeAnimatorController = controller1;
+        }
+        else
+        {
+            animator.runtimeAnimatorController = controller2;
+        }
+    }
+
     public override void TriggerAI()
     {
         System.Random random = new System.Random();
