2f5b6b0fddaa8381b0311cf2bb2f4e2168ef5844
Jaehoon <86000058+soonsoo3595@users.noreply.github.com>
2024-10-14 00:26:21 +0900

    fix : 소환수 카드 수정 + UI Canvas 수정

--- Assets/Scripts/UI/AllyCardList.cs
+++ Assets/Scripts/UI/AllyCardList.cs
@@ -1,17 +1,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
-/*
- * ToDo
- * 소환수 추가
- * 소환수 제거
- * 소환수 소환
-*/ 
 public class AllyCardList : MonoBehaviour
 {
     [SerializeField] List<AllyCard> cards;
-    [SerializeField] Sprite[] cardSprites;
+
+    [HideInInspector] public bool canSummon = false;
 
     private void Start()
     {
@@ -21,7 +15,6 @@ public class AllyCardList : MonoBehaviour
     /// <summary>
     /// 소환수 목록을 받아 소환수 카드를 초기화
     /// </summary>
-    /// <param name="_allies">우치의 소환수 목록(아직 fix된게 없기에 임시임)</param>
     public void Initialize(AllyFormation _allies)
     {
         List<BaseCharacter> characters = _allies.GetAllies();
@@ -30,20 +23,8 @@ public class AllyCardList : MonoBehaviour
         {
             if(i < characters.Count)
             {
-                cards[i].Activate(characters[i], cardSprites[0]);
+                cards[i].Show(characters[i]);
             }
-            else
-            {
-                cards[i].Deactivate(cardSprites[1]);
-            }
-        }
-    }
-
-    public void UpdateList()
-    {
-        foreach(var card in cards)
-        {
-            card.UpdateCard();
         }
     }
     
@@ -53,16 +34,10 @@ public class AllyCardList : MonoBehaviour
         else gameObject.SetActive(true);
     }
 
-    public void SetInteractable(bool _able)
-    {
-        foreach(var card in cards)
-        {
-            card.SetInteractable(_able);
-        }
-    }
-
     public void SelectCard(AllyCard _card)
     {
+        if(!canSummon) return;
+
         MainCharacter mainCharacter = BattleManager.GetInstance.currentCharacter as MainCharacter;
         MC_Summon summon = mainCharacter.SummonSkill;
         BattleManager.GetInstance.SkillSelected(summon);
